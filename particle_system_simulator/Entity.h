4da82602ae9c811d7e37d1f0a8b16eeaaa44053a
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-04-07 04:57:26 +0300

    some ideas and mockups for entt

---
+++ particle_system_simulator/Entity.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <entt/entt.hpp>
+
+/*
+	entt::registry registry;
+    entt::entity entityObject = registry.create();
+    Entity e{registry, entityObject};
+    TranssformComponent tc{e};
+    tc->addComponent<TransformComponent>(1, 2, 3);
+    
+*/
+
+class Entity
+{
+private:
+	entt::registry& registry; // to store the entities
+	entt::entity entityObject; // entity itself
+public:
+	Entity(entt::registry& registry, entt::entity entityObject) : registry(registry), entityObject(entityObject) {}
+
+    template<typename T, typename... Args>
+    T& addComponent(Args&&... args) {
+		return registry.emplace<T>(entityObject, std::forward<Args>(args)...);
+	}
+
+    template<typename T>
+    T& getComponent() {
+        return registry.get<T>(entityObject);
+    }
+
+    template<typename T>
+    bool hasComponent() {
+        return registry.all_of<T>(entityObject);
+    }
+
+    template<typename T>
+    void removeComponent() {
+        registry.remove<T>(entityObject);
+    }
+
+    /*
+    
+        // destroys all the entities in a range
+        auto view = registry.view<a_component, another_component>();
+        registry.destroy(view.begin(), view.end());
+    
+    */
+
+    template<typename... Component>
+    auto getEntitiesWithComponents() {
+		return registry.view<Component...>();
+	}
+
+    /*
+    
+     now there are three different options to loop through the entities from our view
+     1.: a basic lambda which will be called with the given components
+     note: the lambda arguments need to match the components with which we create this view
+     view.each([](const auto &pos, auto &vel) {})
+
+     2.: an extended lambda which also gives us the entity if we need it
+     view.each([](const auto entity, const auto& pos, auto& vel) {});
+
+    3. a for loop by using structured bindings
+    for (auto [entity, pos, vel] : view.each()) {
+    }
+
+    4. a for loop with a forward iterator
+    for (auto entity : view) {
+       auto& vel = view.get<velocity>(entity);
+            
+    }
+    
+    */
+
+    void destroy() {
+		registry.destroy(entityObject);
+	}
+
+    entt::entity getHandle() const { return entityObject; }
+};
+
